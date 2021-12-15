b47fe0ce2772a13792d5531063478c5ec19a735a
theano.xirouchaki <txirouch@vld366.cmpd1.metoffice.gov.uk>
2021-12-15 11:23:34 +0000

    added bash script to mass download the tar files

---
+++ getbengaldata.sh
@@ -0,0 +1,4 @@
+for CYCLONE in BOB01 BOB07 TC01B AKASH SIDR RASHMI AILA VIYARU ROANU MORA FANI 
+do
+    curl -# -o ../Data/$CYCLONE.tar https://zenodo.org/api/files/476d7685-0a99-4f35-a32d-f6951e89dfec/tsens.$CYCLONE.tar.gz
+done

22cb1a448d717a0b537bd63f6254c1efca373d85
Theano Xirouchaki <mo-txirouch@login-01.gw4.metoffice.gov.uk>
2021-12-15 17:38:55 +0000

    added more to bash script

--- getbengaldata.sh
+++ getbengaldata.sh
@@ -1,4 +1,8 @@
+mkdir -p ../RawData
 for CYCLONE in BOB01 BOB07 TC01B AKASH SIDR RASHMI AILA VIYARU ROANU MORA FANI 
 do
-    curl -# -o ../Data/$CYCLONE.tar https://zenodo.org/api/files/476d7685-0a99-4f35-a32d-f6951e89dfec/tsens.$CYCLONE.tar.gz
+if ! test -f "../RawData/$CYCLONE.tar"; then
+echo "it doesn't exist"
+curl -# -o ../RawData/$CYCLONE.tar https://zenodo.org/api/files/476d7685-0a99-4f35-a32d-f6951e89dfec/tsens.$CYCLONE.tar.gz
+fi
 done

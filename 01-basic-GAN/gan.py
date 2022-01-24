b91ec5d900b2479d0e30f01de0e359fa54f1ab53
Hamish Steptoe <hamish.steptoe@metoffice.gov.uk>
2022-01-24 11:59:21 +0000

    Fix global var names

--- 01-basic-GAN/gan.py
+++ 01-basic-GAN/gan.py
@@ -22,11 +22,15 @@ import random
 import sys
 import net_architectures
 
+# Define CUDA env variables
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+
 parser = argparse.ArgumentParser(description='Run GAN With Cyclone Data')
 parser.add_argument('format', metavar='format', type=str, nargs=1, choices=['A','B','C','D','E'], help='data format to use')
 parser.add_argument('net', metavar='net', type=str, nargs=1, choices=["Large_Net", "Small_Net"], help='which network variation to load')
 
-parser.add_argument('path', metavar='path', default="/project/ciid/projects/ML-TC/", type=str, nargs="?", help='the base path to use')
+parser.add_argument('path', metavar='path', default="/lustre/projects/metoffice/ml-tc", type=str, nargs="?", help='the base path to use')
 parser.add_argument('--vars', nargs="+", help="the set of variables to use")
 
 args = parser.parse_args()
@@ -34,45 +38,54 @@ print(args)
 # input()
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
+# Set Global Vars ----------------------------------------------
+
 # Number of workers for dataloader
-workers = 0
+WORKERS = 0
 
 # Batch size during training
-batch_size = 128
+BATCH_SIZE = 128
 
 # Spatial size of training images. All images will be resized to this
 #   size using a transformer.
-image_size = 256
+IMAGE_SIZE = 256
 
 # Number of channels in the training images. For color images this is 3
-nc = 1
+CHANNELS = 1
 
 # Size of z latent vector (i.e. size of generator input)
-nz = 100
+NZ = 100
 
 # Size of feature maps in generator
-ngf = 64
+NGF = 64
 
 # Size of feature maps in discriminator
-ndf = 64
+NDF = 64
 
 # Number of training epochs
-num_epochs = 1000
+NUM_EPOCHS = 1000
 
 # Learning rate for optimizers
-lr = 0.0002
+LR = 0.0002
 
 # Beta1 hyperparam for Adam optimizers
-beta1 = 0.5
+BETA1 = 0.5
 
 # Number of GPUs available. Use 0 for CPU mode.
-ngpu = 1
+NGPU = 2
 
 # Frequency of printing training stats (in steps)
-print_freq = 50
+PRINT_FREQ = 50
 
 # Frequency of testing the generator (in steps)
-test_freq = 500
+TEST_FREQ = 500
+
+# Ouput directory
+OUTPATH = args.path
+
+# Set random seed for reproducibility
+SEED = 357
+# SEED = random.randint(1, 10000) # use if you want new results
 
 def weights_init(m):
     classname = m.__class__.__name__
@@ -96,13 +109,12 @@ def run():
     print(save_path)
     t = open(save_path+"text.txt", "a")
     t.write("Run starting")
-    # Set random seed for reproducibility
-    manualSeed = 357
-    # manualSeed = random.randint(1, 10000) # use if you want new results
-    t.write("Random Seed: "+str(manualSeed))
+    
+    t.write("Random Seed: "+str(SEED))
     t.close()
-    random.seed(manualSeed)
-    torch.manual_seed(manualSeed)
+    random.seed(SEED)
+    torch.manual_seed(SEED)
+    
     # path=str(pathlib.Path(__file__).parent)+"/Data/"
     data=[]
     for f in os.listdir(data_path):
@@ -131,11 +143,11 @@ def run():
     dataset=TensorDataset(data)
 
     # # Create the dataloader
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                            shuffle=True, num_workers=workers)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE,
+                                            shuffle=True, num_workers=WORKERS)
                                             
     # # Decide which device we want to run on
-    device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+    device = torch.device("cuda:0" if (torch.cuda.is_available() and NGPU > 0) else "cpu")
     #print(str(torch.cuda.is_available()))
     t = open(save_path+'text.txt', 'w+')
     t.write(str(torch.cuda.is_available()))
@@ -146,11 +158,11 @@ def run():
     arch=eval('net_architectures.'+args.net[0])
 
     # Create the generator
-    netG = arch.Generator(ngpu,nc, nz, True).to(device)
+    netG = arch.Generator(NGPU,CHANNELS, NZ, True).to(device)
 
     # Handle multi-gpu if desired
-    if (device.type == 'cuda') and (ngpu > 1):
-        netG = nn.DataParallel(netG, list(range(ngpu)))
+    if (device.type == 'cuda') and (NGPU > 1):
+        netG = nn.DataParallel(netG, list(range(NGPU)))
 
     # Apply the weights_init function to randomly initialize all weights
     #  to mean=0, stdev=0.2.
@@ -160,11 +172,11 @@ def run():
     # print(netG)
 
     # Create the Discriminator
-    netD = arch.Discriminator(ngpu,nc, True).to(device)
+    netD = arch.Discriminator(NGPU,CHANNELS, True).to(device)
 
     # Handle multi-gpu if desired
-    if (device.type == 'cuda') and (ngpu > 1):
-        netD = nn.DataParallel(netD, list(range(ngpu)))
+    if (device.type == 'cuda') and (NGPU > 1):
+        netD = nn.DataParallel(netD, list(range(NGPU)))
 
     # Apply the weights_init function to randomly initialize all weights
     #  to mean=0, stdev=0.2.
@@ -178,15 +190,15 @@ def run():
 
     # Create batch of latent vectors that we will use to visualize
     #  the progression of the generator
-    fixed_noise = torch.randn(64, nz, 1, 1, device=device)
+    fixed_noise = torch.randn(64, NZ, 1, 1, device=device)
 
     # Establish convention for real and fake labels during training
     real_label = 1.
     fake_label = 0.
 
     # Setup Adam optimizers for both G and D
-    optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
-    optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+    optimizerD = optim.Adam(netD.parameters(), lr=LR, betas=(BETA1, 0.999))
+    optimizerG = optim.Adam(netG.parameters(), lr=LR, betas=(BETA1, 0.999))
 
     # Training Loop
 
@@ -200,7 +212,7 @@ def run():
     print("Starting Training Loop...")
     t.close()
     # For each epoch
-    for epoch in range(num_epochs):
+    for epoch in range(NUM_EPOCHS):
         t = open(save_path+"text.txt", "a")
         t.write(str(epoch)+str(", "))
         t.close()
@@ -226,7 +238,7 @@ def run():
 
             ## Train with all-fake batch
             # Generate batch of latent vectors
-            noise = torch.randn(b_size, nz, 1, 1, device=device)
+            noise = torch.randn(b_size, NZ, 1, 1, device=device)
             # Generate fake image batch with G
             fake = netG(noise)
             # print(fake.shape)
@@ -260,9 +272,9 @@ def run():
             optimizerG.step()
 
             # Output training stats
-            if iters % print_freq == 0:
+            if iters % PRINT_FREQ == 0:
                 print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-                    % (epoch, num_epochs, i, len(dataloader),
+                    % (epoch, NUM_EPOCHS, i, len(dataloader),
                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
             # Save Losses for plotting later
@@ -270,7 +282,7 @@ def run():
             D_losses.append(errD.item())
 
             # Check how the generator is doing by saving G's output on fixed_noise
-            if (iters % test_freq == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
+            if (iters % TEST_FREQ == 0) or ((epoch == NUM_EPOCHS-1) and (i == len(dataloader)-1)):
                 with torch.no_grad():
                     fake = netG(fixed_noise).detach().cpu().numpy()
                 img_list.append(fake)
